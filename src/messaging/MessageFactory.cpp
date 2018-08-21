44206290097737204c49f87147416de57841e2ce
geekymoose <constantin.masson@umontreal.ca>
2018-08-21 19:20:23 -0400

    Update MessageFactory with all current existing messages

--- src/messaging/MessageFactory.cpp
+++ src/messaging/MessageFactory.cpp
@@ -10,12 +10,19 @@ std::unique_ptr<Message> MessageFactory::newMessage(const int type) const {
     switch(type) {
         case MSG_DEBUG:
             return std::unique_ptr<Message>(new MsgDebug());
+        case MSG_ERROR:
+            return std::unique_ptr<Message>(new MsgError());
+        case MSG_CONNECTION_REQ:
+            return std::unique_ptr<Message>(new MsgConnectionRequest());
+        case MSG_CONNECTION_SUCCESS:
+            return std::unique_ptr<Message>(new MsgConnectionSuccess());
 
 
         // ---------------------------------------------------------------------
         // Miscellaneous Messages
         // ---------------------------------------------------------------------
         default:
+
             return nullptr;
     }
 }
